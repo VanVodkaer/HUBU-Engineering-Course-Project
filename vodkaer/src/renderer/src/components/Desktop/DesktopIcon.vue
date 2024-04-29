4df94ddf1d52f9f9b8b941eae42cbebc77b5c6a8
VanVodkaer <mail@vodkaer.com>
2024-04-29 13:56:10 +0800

    软件框架搭建

---
+++ vodkaer/src/renderer/src/components/Desktop/DesktopIcon.vue
@@ -0,0 +1,27 @@
+<script setup></script>
+
+<template>
+  <div class="desktop-icon">
+    <img src="1.png" alt="Folder" />
+    <span>文件夹</span>
+  </div>
+</template>
+
+<style scoped>
+.desktop-icon {
+  width: 80px;
+  text-align: center;
+  margin: 20px;
+  display: inline-block;
+  position: relative;
+}
+.desktop-icon img {
+  width: 100%;
+}
+.desktop-icon span {
+  display: block;
+  text-align: center;
+  color: white;
+  text-shadow: 0px 0px 5px rgba(0, 0, 0, 0.7);
+}
+</style>
