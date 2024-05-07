7b7659ff75cd52e405374ab9dab4d308df8a102f
cjxqq123 <cjx1234567555@qq.com>
2024-05-07 19:27:55 +0800

    改分支

---
+++ cjxqq123/calculator.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int a, b = 0;
+	char c;
+	cout << "请输入你要计算的数字" << endl;
+	cin >> a >> b;
+	cout << "请输入运算符" << endl;
+	cin >> c;
+	int d;
+	if (c == '+') {
+		d = a + b;
+		cout << "a+b=" << d << endl;
+	}
+	if (c == '-') {
+		d = a-b;
+		cout << "a-b=" << d << endl;
+	}
+	if (c == '*') {
+		d = a * b;
+		cout << "a*b=" << d << endl;
+		
+	}
+	if (c == '/') {
+		d = a / b;
+		cout << "a/b=" << d << endl;
+	}
+	system("pause");
+	return 0;
+}
