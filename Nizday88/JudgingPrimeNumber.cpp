cdfacc669f7b257b00820e3e76a9e54cfdee685a
Nizday88 <167863076+Nizday88@users.noreply.github.com>
2024-05-14 13:56:35 +0800

    Update JudgingPrimeNumber.cpp
    增加非整数检测

--- Nizday88/JudgingPrimeNumber.cpp
+++ Nizday88/JudgingPrimeNumber.cpp
@@ -1,31 +1,50 @@
-#include<iostream>
-#include<cmath> 
+#include <iostream>
+#include <cmath>
+#include <limits> 
 using namespace std;
 
 int main()
 {
-	int x,flag=0; 
-	cout<<"可输入非正数以结束程序"<<endl; 
-	cout<<"请输入一个数：";
-	cin>>x;
-	while(x>0)
-	{
-		for (int i=2;i<=sqrt(x);i++)
+    int x;
+    bool flag; 
+    cout << "可输入非正数以结束程序" << endl;
+
+    while (true)
+    {
+        cout << "请输入一个数：";
+        cin >> x;
+
+        // 检测输入是否为有效整数
+        if (cin.fail()) 
 		{
-			if(x%i==0)
-			{
-				flag=1; 
-				cout<<x<<"不是一个质数"<<endl;
-				break;
-			}
-			flag=0; 
-		}
-		if(flag==0)
-		{
-			cout<<x<<"是一个质数"<<endl; 
-		}
-		cout<<"请输入一个数：";
-		cin>>x;
-	}
-	return 0;
-} 
+            cin.clear();  // 清除错误标志
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');  // 忽略输入流中的所有字符，直到遇到换行符
+            cout << "请输入一个有效的整数。" << endl;
+            continue;
+        }
+
+        // 如果输入的是非正数，结束程序
+        if (x <= 0) {
+            break;
+        }
+
+        flag = false;  // 重置标志变量
+
+        for (int i = 2; i <= sqrt(x); i++)
+        {
+            if (x % i == 0)
+            {
+                flag = true;
+                cout << x << " 不是一个质数" << endl;
+                break;
+            }
+        }
+
+        if (!flag)
+        {
+            cout << x << " 是一个质数" << endl;
+        }
+    }
+
+    return 0;
+}
